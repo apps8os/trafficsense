63e2e68e50e29a78699fbb11626d280a8396f1bd
Zhen-Huan Hwang <praise.digital@gmail.com>
2014-01-17 11:09:37 +0200

    added a JSON template and a (simplified) journey

---
+++ experiment/json/template.js
@@ -0,0 +1,21 @@
+{
+ "date": "",
+ "start": "",
+ "dest": "",
+ "arrivalTime": "",
+ "segments": [
+  {
+   "startTime": "",
+   "startPoint": "",
+   "mode": "",
+   "waypoints": [
+    {
+     "time": "",
+     "name": "",
+     "stopCode": ""
+    }
+   ]
+  }
+ ]
+}
+
