61dcf9ef743925b592a921cc66e475e2b551967d
traffisense <traffisense@traffisense3.cs.hut.fi>
2014-01-22 11:04:00 +0200

    Android skeleton for the First Prototype

---
+++ FirstProtoApp/src/org/apps8os/trafficsense/first/MainActivity.java
@@ -0,0 +1,27 @@
+package org.apps8os.trafficsense.first;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
