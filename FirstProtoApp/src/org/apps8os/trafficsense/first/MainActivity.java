2bf0808f938e492ee68912bb0c951cd1a0fe2359
Zhen-Huan Hwang <praise.digital@gmail.com>
2014-01-23 14:59:06 +0200

    contextlogger3 plugged into the first prototype

--- FirstProtoApp/src/org/apps8os/trafficsense/first/MainActivity.java
+++ FirstProtoApp/src/org/apps8os/trafficsense/first/MainActivity.java
@@ -1,5 +1,7 @@
 package org.apps8os.trafficsense.first;
 
+import org.apps8os.contextlogger.android.integration.MonitoringFrameworkAgent;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -10,10 +12,23 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// Start ContextLogger3
+		// Get instance of MonitoringFrameworkAgent
+		MonitoringFrameworkAgent mfAgent = MonitoringFrameworkAgent.getInstance();
+		// Start Monitoring Framework using an instance of android.content.Context
+		mfAgent.start(this);
 	}
 	
 	@Override
 	protected void onDestroy() {
+		
+		// Stop ContextLogger3
+		// Get instance of MonitoringFrameworkAgent
+		MonitoringFrameworkAgent mfAgent = MonitoringFrameworkAgent.getInstance();
+		// Stop Monitoring Framework
+		mfAgent.stop(this);
+		
 		super.onDestroy();
 	}
 
