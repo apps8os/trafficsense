97a4ef85c52986db179647aaf05813fa78e0499e
Javier López Lumbreras <loplumja@hotmail.com>
2014-01-24 16:57:35 +0200

    First Alarm Version

---
+++ experiment/AndroidAlarmService/src/com/exercise/AndroidAlarmService/ResponseReceiver.java
@@ -0,0 +1,16 @@
+package com.exercise.AndroidAlarmService;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class ResponseReceiver extends BroadcastReceiver{
+	 public static final String ACTION_RESP = "MESSAGE_PROCESSED";
+		   @Override
+		    public void onReceive(Context context, Intent intent) {
+			   Toast.makeText(context, "Message", Toast.LENGTH_LONG).show();
+		   };
+		    
+
+}
