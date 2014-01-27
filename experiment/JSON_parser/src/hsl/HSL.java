6cf26f2e93a782eca885c7172b2de6420acef489
Catarina Moura <catarinapachecomoura@gmail.com>
2014-01-27 13:33:36 +0200

    JSON

---
+++ experiment/JSON_parser/src/hsl/HSL.java
@@ -0,0 +1,14 @@
+package hsl;
+
+public class HSL {
+
+	public static void main(String[] args) {
+		
+		JSON file = new JSON();
+		file.parsingFile("/Users/catarinamoura/Desktop/trafficsense/experiment/json/sample.txt") ;
+		
+		
+
+	}
+
+}
