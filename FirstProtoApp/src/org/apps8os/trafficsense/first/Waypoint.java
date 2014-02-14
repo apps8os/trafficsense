1d892f03bbb96de1c6d8b1bd337c28030a6dd76f
Zhen-Huan Hwang <praise.digital@gmail.com>
2014-02-14 17:12:43 +0200

    clean up 1st prototype

--- FirstProtoApp/src/org/apps8os/trafficsense/first/Waypoint.java
+++ FirstProtoApp/src/org/apps8os/trafficsense/first/Waypoint.java
@@ -7,8 +7,6 @@ public class Waypoint {
 	private String time;
 	private String name;
 	private String stopCode;
-	private String xCord;
-	private String yCord;
 	
 	public String getWaypointTime(){
 		return time;
@@ -40,15 +38,12 @@ public class Waypoint {
 	public void setWaypoint (JsonObject waypoint){
 		setWaypointTime (waypoint.get("time").getAsString());
 		setWaypointName (waypoint.get("name").getAsString());
-		Request req = new Request();
 		if (waypoint.has("stopCode")) {
 			setWaypointStopCode(waypoint.get("stopCode").getAsString());		
-			req.getStopInfo("", stopCode);		
 		} 
-			else { 
+		else { 
 				// TODO: what should this be if there is no stopCode ???
 				setWaypointStopCode("0000");
-				req.getGeocoding("00000010", "", name, "street");
 		}
 		
 	}
