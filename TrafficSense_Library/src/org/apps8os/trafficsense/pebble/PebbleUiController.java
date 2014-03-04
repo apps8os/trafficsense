f2f8468df4c32b2ecea6c7af6e007b7d49648c02
Zhen-Huan Hwang <praise.digital@gmail.com>
2014-03-04 18:41:07 +0200

    some documentation for PebbleuiController

--- TrafficSense_Library/src/org/apps8os/trafficsense/pebble/PebbleUiController.java
+++ TrafficSense_Library/src/org/apps8os/trafficsense/pebble/PebbleUiController.java
@@ -1,33 +1,39 @@
 package org.apps8os.trafficsense.pebble;
 
-
 import java.util.ArrayList;
 
 import org.apps8os.trafficsense.core.Route;
 import org.apps8os.trafficsense.core.Segment;
 import org.apps8os.trafficsense.core.Waypoint;
 
-
 /**
- * Controls the Pebble UI
- *
+ * Class for Pebble user interface controller.
  */
 public class PebbleUiController {
-	PebbleCommunication mPblCom;
-	Route mRoute;
+	private PebbleCommunication mPblCom;
+	private Route mRoute;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param comm PebbleCommunication link.
+	 * @param route route object to use.
+	 */
 	public PebbleUiController(PebbleCommunication comm, Route route) {
 		mPblCom = comm;
 		mRoute = route;
 	}
-	
-	/*
-	 * Should be called whenever the segment changes. Initializes
-	 * the list of stops shown in Pebble screen to show the 1st, 2nd
-	 * and the last stop of the segment.
+
+	/**
+	 * Sets Pebble UI to the first two and the last stop of current segment.
+	 * 
+	 * Should be called whenever the segment changes. Sets Pebble UI to the
+	 * first two and the last stop of current segment.
 	 */
 	public void initializeList() {
 		Segment newSegment = mRoute.getCurrentSegment();
 		ArrayList<Waypoint> waypoints = new ArrayList<Waypoint>();
+		
 		// Add the waypoints from the segment to the list
 		//int maxIndex = Math.min(newSegment.getWaypointList().size() - 1, PebbleCommunication.NUM_STOPS - 1 );
 		for (int i = 0; i < PebbleCommunication.NUM_STOPS - 1; i++) {
@@ -38,21 +44,27 @@ public class PebbleUiController {
 			}
 		}
 		waypoints.add(newSegment.getLastWaypoint());
-		if (newSegment.getLastWaypoint() == null)
-			System.out.println("DBG last waypoint was null");
+		if (newSegment.getLastWaypoint() == null) {
+			System.out.println("DBG initializeList last waypoint was null");
+		}
 		// Send the waypoints to Pebble
 		for (int i = 0; i < PebbleCommunication.NUM_STOPS; i++) {
 			mPblCom.sendWaypoint(waypoints.get(i), i);
 		}
 	}
-	
+
+	/**
+	 * Scrolls Pebble UI NUM_STOPS stops forward.
+	 */
 	public void updateList() {
-		System.out.println("updatelist s " + mRoute.getCurrentIndex());
-		System.out.println("updatelist w " + mRoute.getCurrentSegment().getCurrentIndex());
+		System.out.println("DBG updateList s " + mRoute.getCurrentIndex());
+		System.out.println("DBG updateList w "
+				+ mRoute.getCurrentSegment().getCurrentIndex());
 		if (mRoute.getCurrentSegment().getCurrentIndex() == 0) {
 			return;
 		}
-		int newWaypoint = mRoute.getCurrentSegment().getCurrentIndex() + PebbleCommunication.NUM_STOPS - 2;
+		int newWaypoint = mRoute.getCurrentSegment().getCurrentIndex()
+				+ PebbleCommunication.NUM_STOPS - 2;
 		Waypoint waypoint;
 		if (newWaypoint > mRoute.getCurrentSegment().getWaypointList().size() - 1) {
 			waypoint = null;
@@ -61,7 +73,10 @@ public class PebbleUiController {
 		}
 		mPblCom.updateList(waypoint);
 	}
-	
+
+	/**
+	 * Triggers a 'Get off' alarm on Pebble.
+	 */
 	public void alarmGetOff() {
 		mPblCom.sendMessage("Alarm", "Get off on the next stop!");
 	}
